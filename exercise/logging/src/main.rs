81a14a30c01616a14a640a14134ad7f0f38e641b
Vivek Yadav <vivek.yadav472@gmail.com>
2022-10-01 17:04:05 +0530

    logging using log and env_logger

--- exercise/logging/src/main.rs
+++ exercise/logging/src/main.rs
@@ -1,17 +1,7 @@
-// START IN lib.rs!!!
-
 use frogger::Frog;
 
-// You did #1-#6 in lib.rs already, right?
-//
-// 7. Update Cargo.toml to add the `env_logger` dependency
-
 fn main() {
-    // 8. Initialize env_logger using the init() function at the top level of the library
-
-    // 9. Run this program with `cargo run` and take a look at the default output.
-    // - Now run it again with an explicit log level, like `RUST_LOG=info cargo run`
-    // - or `RUST_LOG=trace cargo run`
+    env_logger::init();
     let mut skippy = Frog::new();
     skippy.hop();
     skippy.hop();
@@ -20,7 +10,4 @@ fn main() {
     skippy.hop();
     skippy.sleep();
     skippy.sleep();
-
-    // Challenge: Go back to lib.rs and set the `target: ` argument for each logging call to be the
-    // path to the function.  For example, `Frog::new`
 }
