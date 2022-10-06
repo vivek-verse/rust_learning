0a174734e6f2dfa572a8765fd91a5d8a102cc126
Vivek Yadav <vivek.yadav472@gmail.com>
2022-10-06 17:56:06 +0530

    simple search, just to learn rust syntax

---
+++ projecs/just_a_search/src/main.rs
@@ -0,0 +1,20 @@
+use just_a_search::run;
+use just_a_search::Config;
+use std::env;
+use std::process;
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
