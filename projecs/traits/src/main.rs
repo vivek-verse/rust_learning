e6577805cc8e9c6c40fe0235c33366a8bf5aa4ff
Vivek Yadav <vivek.yadav472@gmail.com>
2022-10-06 06:33:24 +0530

    traits simplified

--- projecs/traits/src/main.rs
+++ projecs/traits/src/main.rs
@@ -1,81 +1,54 @@
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum Cake {
-    Chocolate,
-    MapleBacon,
-    Spice,
+pub struct NewsArticle {
+    pub author: String,
+    pub headline: String,
+    pub content: String,
 }
 
-#[derive(Debug)]
-pub struct Party {
-    pub at_restaurant: bool,
-    pub num_people: u8,
-    pub cake: Cake,
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("Read more from {}...", self.summarize_author())
+    }
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("{}", self.author)
+    }
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
 }
 
 fn main() {
-    let cake = Cake::Spice;
-    admire_cake(cake);
-
-    match cake {
-        Cake::Chocolate => println!("The name's Chocolate. Dark...Chocolate."),
-        Cake::MapleBacon => println!("Dreams do come true!"),
-        Cake::Spice => println!("Great, let's spice it up!"),
-    }
-
-    impl Default for Party {
-        fn default() -> Self {
-            Party {
-                at_restaurant: true,
-                num_people: 8,
-                cake: Cake::Chocolate,
-            }
-        }
-    }
-
-    impl PartialEq for Party {
-        fn eq(&self, other: &Self) -> bool {
-            self.cake == other.cake
-        }
-    }
-
-    impl From<&Party> for Cake {
-        fn from(party: &Party) -> Self {
-            party.cake
-        }
-    }
-
-    println!("The default Party is\n{:#?}", Party::default());
-
-    let party = Party {
-        cake: Cake::MapleBacon,
-        ..Default::default()
+    let tweet = Tweet {
+        username: String::from("@johndoe"),
+        content: String::from("Hello World!"),
+        reply: false,
+        retweet: false,
     };
 
-    println!("Yes! My party has my favorite {:?} cake!", party.cake);
-
-    let other_party = Party {
-        at_restaurant: false,
-        num_people: 235,
-        cake: Cake::MapleBacon,
+    let article = NewsArticle {
+        author: String::from("John Doe"),
+        headline: String::from("The Sky is Falling!"),
+        content: String::from("The sky is not actually falling."),
     };
 
-    if party == other_party {
-        println!("Your party is just like mine!");
-    }
-
-    smell_cake(&party);
-
-    println!(
-        "Yum! I'm eating this cake: {:?}. Oops, I dropped it on the floor.",
-        party.cake
-    );
-    drop(cake);
-}
-
-pub fn admire_cake(cake: Cake) {
-    println!("What a nice {:?} cake! 🎂", cake);
-}
-
-pub fn smell_cake<T: Into<Cake>>(something: T) {
-    println!("Hmm...something smells like a {:?} cake!", something.into());
+    println!("Tweet summary: {}", tweet.summarize());
+    println!("Article summary: {}", article.summarize());
 }
