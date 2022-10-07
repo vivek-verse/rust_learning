6ac307d7d761998202d29942c6d5d2b0d286e4dd
Vivek Yadav <vivek.yadav472@gmail.com>
2022-10-07 13:48:20 +0530

    box pointerrrrrrrr

---
+++ projecs/box_pointer/src/main.rs
@@ -0,0 +1,14 @@
+//Box pointer is used when size is not fixed, in recursize cases
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
+fn main() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("{:?}", list);
+}
