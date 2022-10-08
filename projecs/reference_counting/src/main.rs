8f05ca0e560f1ed820b69b4fae4e4276e1b5448e
Vivek Yadav <vivek.yadav472@gmail.com>
2022-10-08 07:16:31 +0530

    reference counting

---
+++ projecs/reference_counting/src/main.rs
@@ -0,0 +1,16 @@
+use std::rc::Rc;
+
+// Rc is used when 2 or more are referencing the same thing
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
+fn main() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
+}
