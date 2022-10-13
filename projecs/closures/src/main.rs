feff7d17b3cb9f41e6dc81c0ebe0003451c309de
Vivek Yadav <vivek.yadav472@gmail.com>
2022-10-13 13:46:24 +0530

    closures revisited

--- projecs/closures/src/main.rs
+++ projecs/closures/src/main.rs
@@ -1,59 +1,63 @@
-use std::thread;
-use std::time::Duration;
+/*
+    3 traits
+    Fn - immutable
+    FnMut - mutable reference
+    FnOnce - By move (taken total ownership)
+*/
 
-struct Cacher<T>
+fn add_five<F>(mut func: F)
 where
-    T: Fn(u32) -> u32,
+    F: FnMut(i32),
 {
-    calculation: T,
-    value: Option<u32>,
-}
-
-impl<T> Cacher<T>
-where
-    T: Fn(u32) -> u32,
-{
-    fn new(calculation: T) -> Cacher<T> {
-        Cacher {
-            calculation,
-            value: None,
-        }
-    }
-
-    fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
-                v
-            }
-        }
-    }
-}
-
-fn generate_workout(intensity: u32, random_number: u32) {
-    let mut cached_result = Cacher::new(|num| {
-        println!("calculating slowly...");
-        thread::sleep(Duration::from_secs(2));
-        num
-    });
-
-    if intensity < 25 {
-        println!("Today, do {} pushups!", cached_result.value(intensity));
-        println!("Next, do {} situps!", cached_result.value(intensity));
-    } else {
-        if random_number == 3 {
-            println!("Take a break today! Remember to stay hydrated!");
-        } else {
-            println!("Today, run for {} minutes!", cached_result.value(intensity))
-        }
-    }
+    func(5)
 }
 
 fn main() {
-    let simulated_intensity = 10;
-    let simulated_random_number = 7;
+    let mut str = "The quick brown fox".to_string();
+    let clzr1 = |x| println!("{} {}", str, x);
+    clzr1("jumps over the lazy dog"); //Fn Trait
 
-    generate_workout(simulated_intensity, simulated_random_number);
+    let mut clzr2 = |x| str.push_str(x);
+    clzr2(" jumps over the lazt dog");
+    println!("{}", str); //FnMut trait
+
+    let clzr3 = || drop(str);
+
+    let mut num = 6;
+
+    add_five(|x| {
+        num += x;
+        println!("{}", num)
+    });
+
+    let car_collection = vec![
+        ("Thunderbird", 1960),
+        ("Cobra", 1966),
+        ("GT", 1967),
+        ("Mustang Grande", 1969),
+    ];
+
+    let ford_models = ClassicCars {
+        make: "Ford",
+        models: car_collection,
+    };
+
+    ford_models.smart_get(|x| {
+        let res: Vec<_> = x.into_iter().filter(|x| x.1 > 1960).collect();
+        println!("results {:?}", res);
+    });
+}
+
+struct ClassicCars {
+    make: &'static str,
+    models: Vec<(&'static str, i32)>,
+}
+
+impl ClassicCars {
+    fn smart_get<F>(&self, f: F)
+    where
+        F: Fn(&Vec<(&'static str, i32)>),
+    {
+        f(&self.models)
+    }
 }
