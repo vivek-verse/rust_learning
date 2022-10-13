329378621bef1e5bece5ae9880985961df6c5a7e
Vivek Yadav <vivek.yadav472@gmail.com>
2022-10-13 16:11:01 +0530

    iterators revisited

--- projecs/iterators/src/main.rs
+++ projecs/iterators/src/main.rs
@@ -4,6 +4,29 @@ struct Shoe {
     style: String,
 }
 
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Self {
+        Self { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
 fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
     shoes.into_iter().filter(|s| s.size == shoe_size).collect()
 }
@@ -27,4 +50,15 @@ fn main() {
     let in_my_size = shoes_in_my_size(shoes, 10);
 
     println!("in my size shoes {:?}", in_my_size);
+}
+
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
 }
