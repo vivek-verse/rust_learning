f4d4b4e68ece361f640adbc9524b61f3c5535961
Vivek Yadav <vivek.yadav472@gmail.com>
2022-10-14 21:35:18 +0530

    interior mutability revisited

--- projecs/interior_mutability/src/lib.rs
+++ projecs/interior_mutability/src/lib.rs
@@ -25,13 +25,13 @@ where
         let percentage_of_max = self.value as f64 / self.max as f64;
 
         if percentage_of_max >= 1.0 {
-            self.messenger.send("Error you are over your quota!");
+            self.messenger.send("Error: You are over your quota!");
         } else if percentage_of_max >= 0.9 {
             self.messenger
-                .send("Urgent warning: You've used up over 90% of quota")
+                .send("Urgent warning: You've used up over 90% of your quota!");
         } else if percentage_of_max >= 0.75 {
             self.messenger
-                .send("Warning: You've used up over 75% of your quota");
+                .send("Warnig: You've used up over 75% of your quota!");
         }
     }
 }
@@ -40,31 +40,35 @@ where
 mod tests {
     use super::*;
     use std::cell::RefCell;
+    use std::rc::Rc;
 
     struct MockMessenger {
-        sent_messages: RefCell<Vec<String>>,
+        sent_messages: Rc<RefCell<Vec<String>>>,
     }
 
     impl MockMessenger {
         fn new() -> MockMessenger {
             MockMessenger {
-                sent_messages: RefCell::new(vec![]),
+                sent_messages: Rc::new(RefCell::new(vec![])),
             }
         }
     }
 
     impl Messenger for MockMessenger {
         fn send(&self, message: &str) {
-            self.sent_messages.borrow_mut().push(String::from(message));
+            let one_borrow = &self.sent_messages;
+            let two_borrow = &self.sent_messages;
+
+            one_borrow.borrow_mut().push(String::from(message));
+            two_borrow.borrow_mut().push(String::from(message));
         }
     }
 
     #[test]
-    fn it_send_an_over_75_percent_warning_message() {
+    fn it_sends_an_over_75_percent_warning_message() {
         let mock_messenger = MockMessenger::new();
         let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
         limit_tracker.set_value(80);
-
-        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 2);
     }
 }
