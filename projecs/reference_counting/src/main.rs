b965d6071e5c8b32ca8dd9050cc6eaf642976dc7
Vivek Yadav <vivek.yadav472@gmail.com>
2022-10-14 08:18:27 +0530

    reference counting revisit

--- projecs/reference_counting/src/main.rs
+++ projecs/reference_counting/src/main.rs
@@ -11,6 +11,13 @@ use crate::List::{Cons, Nil};
 
 fn main() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let _b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let _c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    // b and c both store the same reference
 }
