422ac64f4de1b39fdc132bcda1a30a4f508446b0
Vivek Yadav <vivek.yadav472@gmail.com>
2022-10-30 19:13:13 +0530

    iterators simplified

--- projecs/iterators/src/main.rs
+++ projecs/iterators/src/main.rs
@@ -1,64 +1,44 @@
-#[derive(PartialEq, Debug)]
-struct Shoe {
-    size: u32,
-    style: String,
+struct Person {
+    first_name: String,
+    last_name: String,
+    occupation: String,
 }
 
-struct Counter {
-    count: u32,
+struct PersonIterator {
+    values: Vec<String>,
 }
 
-impl Counter {
-    fn new() -> Self {
-        Self { count: 0 }
-    }
-}
-
-impl Iterator for Counter {
-    type Item = u32;
+impl Iterator for PersonIterator {
+    type Item = String;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.count < 5 {
-            self.count += 1;
-            Some(self.count)
+        if self.values.is_empty() {
+            return None;
         } else {
-            None
+            Some(self.values.remove(0))
         }
     }
 }
 
-fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
-    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+impl IntoIterator for Person {
+    type Item = String;
+    type IntoIter = PersonIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+        PersonIterator {
+            values: vec![self.first_name, self.last_name, self.occupation],
+        }
+    }
 }
 
 fn main() {
-    let shoes = vec![
-        Shoe {
-            size: 10,
-            style: String::from("sneaker"),
-        },
-        Shoe {
-            size: 13,
-            style: String::from("sandal"),
-        },
-        Shoe {
-            size: 10,
-            style: String::from("boot"),
-        },
-    ];
+    let p = Person {
+        first_name: "Vivek".to_owned(),
+        last_name: "Yadav".to_owned(),
+        occupation: "Software Engineer".to_owned(),
+    };
 
-    let in_my_size = shoes_in_my_size(shoes, 10);
-
-    println!("in my size shoes {:?}", in_my_size);
-}
-
-#[test]
-fn calling_next_directly() {
-    let mut counter = Counter::new();
-    assert_eq!(counter.next(), Some(1));
-    assert_eq!(counter.next(), Some(2));
-    assert_eq!(counter.next(), Some(3));
-    assert_eq!(counter.next(), Some(4));
-    assert_eq!(counter.next(), Some(5));
-    assert_eq!(counter.next(), None);
+    for item in p {
+        println!("{}", item);
+    }
 }
