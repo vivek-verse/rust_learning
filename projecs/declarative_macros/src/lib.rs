9da402ef4965018d04104767d5e529b9e7765d33
Vivek Yadav <vivek.yadav472@gmail.com>
2022-10-31 16:22:12 +0530

    Pretty cool stuff

--- projecs/declarative_macros/src/lib.rs
+++ projecs/declarative_macros/src/lib.rs
@@ -10,3 +10,18 @@ macro_rules! vivek {
         }
     };
 }
+
+#[macro_export]
+macro_rules! map {
+    // $ [identifier] : [fragment-specifier]
+    ($key:ty, $val:ty) => {{
+        let map: HashMap<$key, $val> = HashMap::new();
+        map
+    }};
+
+    ($($key:expr => $val:expr), *) => {{
+        let mut map = HashMap::new();
+        $(map.insert($key, $val);)*
+        map
+    }};
+}
--- projecs/declarative_macros/src/main.rs
+++ projecs/declarative_macros/src/main.rs
@@ -1,6 +1,20 @@
-use declarative_macros::vivek;
+use declarative_macros::*;
+use std::collections::HashMap;
 fn main() {
     let v1: Vec<u32> = vivek![1, 2, 3];
-
     println!("v1 is {:?}", v1);
+    let mut scores = map!(String, i32);
+    scores.insert("Red team".to_owned(), 3);
+    scores.insert("Blue team".to_owned(), 5);
+    scores.insert("Green team".to_owned(), 2);
+
+    println!("{scores:?}");
+
+    let scores2 = map!(
+        "Red team".to_owned() => 3,
+        "Blue team".to_owned() => 5,
+        "Green team".to_owned() => 2
+    );
+
+    println!("{scores2:?}");
 }
